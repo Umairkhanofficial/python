b23d0f951d2a707843a4295705aa6f51a1ecf2c0
umairkhanofficial <umairkhansadiq@gmail.com>
2023-10-20 11:51:15 -0700

    class 4(list and slicing)

---
+++ class4/class4.py
@@ -0,0 +1,7 @@
+#from typing import Any
+#error ouccr list type is str and we giving the 5 elemnt
+names: list [str] = ["Ali","umer","umair", 5]
+
+print(type(names))
+print(type(names[1]))
+print(f"founder of ai {names[2].upper()}")
