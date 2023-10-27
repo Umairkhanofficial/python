572f055f831b6629e32c6efe63d69d3e0c53d1a9
umairkhanofficial <umairkhansadiq@gmail.com>
2023-10-27 11:17:03 -0700

    class6 if elif chaining

---
+++ class6/class6.py
@@ -0,0 +1,10 @@
+if False:
+    print("if ture block")
+elif True:
+    print("ellif 1st block")
+elif True:
+    print("ellif 2nd block")
+elif True:
+    print("ellif 3nd block")
+else:
+    print("else block")
